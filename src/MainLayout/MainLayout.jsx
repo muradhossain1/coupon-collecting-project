aab861e780dc8ec37be1eabf0327f33277885f80
muradhossain1 <muradssq12@gmail.com>
2024-11-19 00:56:22 +0600

    Setup a footer section

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,12 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Navber from "../Componentes/Navber";
+import Footer from "../Componentes/Footer";
 
 
 const MainLayout = () => {
     return (
-        <div className="md:w-11/12 mx-auto">
-            <Navber></Navber>
-            <Outlet></Outlet>
+        <div>
+            <header className="md:w-11/12 mx-auto">
+                <Navber></Navber>
+            </header>
+            <main className="md:w-11/12 mx-auto">
+                <Outlet></Outlet>
+            </main>
+            <footer className="mt-12">
+                <Footer></Footer>
+            </footer>
         </div>
     );
 };
